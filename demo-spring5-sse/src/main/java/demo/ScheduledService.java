f51409fd66c4b225b21fe9d426c6237baa10842f
jhines2k7 <jameshines10@gmail.com>
2017-05-31 11:50:19 -0400

    Experimenting with moving the Integration Flow bean in different places

--- demo-spring5-sse/src/main/java/demo/ScheduledService.java
+++ demo-spring5-sse/src/main/java/demo/ScheduledService.java
@@ -1,20 +1,40 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
+import org.springframework.messaging.SubscribableChannel;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
 
+import java.io.File;
 import java.util.Date;
 
 @Component
 public class ScheduledService {
 
     private final EmitterProcessor<ServerSentEvent<Message>> emitter;
+
+    @Bean
+    SubscribableChannel filesChannel() {
+        return MessageChannels.publishSubscribe().get();
+    }
+
+    @Bean
+    IntegrationFlow integrationFlow(@Value("${input-dir:file://C:\\Users\\Jhines\\Desktop\\sse-in}") File in) {
+        return IntegrationFlows.from(Files.inboundAdapter(in).autoCreateDirectory(true),
+                poller -> poller.poller(spec -> spec.fixedRate(1000L)))
+                .transform(File.class, File::getAbsolutePath)
+                .channel(filesChannel())
+                .get();
+    }
 
     public ScheduledService() {
         emitter = EmitterProcessor.create();
@@ -24,16 +44,18 @@ public class ScheduledService {
         return emitter.log();
     }
 
-    @Scheduled(fixedRate = 1000)
+    //@Scheduled(fixedRate = 1000)
     void timerHandler() {
-        try {
-            emitter.onNext(
-                ServerSentEvent.builder(
-                    new Message(new Date().toString())
-                ).build()
-            );
-        } catch (Exception e) {
-            emitter.onError(e);
-        }
+        filesChannel().subscribe(msg -> {
+            try {
+                emitter.onNext(
+                    ServerSentEvent.builder(
+                        new Message(new Date().toString())
+                    ).build()
+                );
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        });
     }
 }
--- demo-spring5-sse/src/main/java/demo/SseController.java
+++ demo-spring5-sse/src/main/java/demo/SseController.java
@@ -1,21 +1,13 @@
 package demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.dsl.channel.MessageChannels;
-import org.springframework.integration.file.dsl.Files;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.SubscribableChannel;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
-import java.io.File;
 import java.io.IOException;
 
 @RestController
@@ -27,28 +19,14 @@ public class SseController {
     @Autowired
     private ReactiveService reactiveService;
 
-    @Bean
-    SubscribableChannel filesChannel() {
-        return MessageChannels.publishSubscribe().get();
-    }
-
-    @Bean
-    IntegrationFlow integrationFlow(@Value("${input-dir:file://C:\\Users\\James\\Desktop\\sse-in}") File in) {
-        return IntegrationFlows.from(Files.inboundAdapter(in).autoCreateDirectory(true),
-                poller -> poller.poller(spec -> spec.fixedRate(1000L)))
-                .transform(File.class, File::getAbsolutePath)
-                .channel(filesChannel())
-                .get();
-    }
-
-    @GetMapping(value = "/files/{name}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    Flux<String> files(@PathVariable String name) {
-        return Flux.create(
-                s -> {
-                    MessageHandler handler = msg -> s.next(String.class.cast(msg.getPayload()));
-                    filesChannel().subscribe(handler);
-                    s.complete();
-                });
+    @GetMapping(path = "/sse/files", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<Message>> getFiles() throws IOException{
+        /*return Flux.create(
+            s -> {
+                filesChannel().subscribe(msg -> s.next(String.class.cast(msg.getPayload())));
+                s.complete();
+            });*/
+        return scheduledService.getMessages();
     }
 
     @GetMapping(name = "/sse/infinite", produces = "text/event-stream")
